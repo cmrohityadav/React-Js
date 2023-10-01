277bffc19df8852dfe94348c09fbd424cc9c2321
cmrohityadav <cmrohityadav23@gmail.com>
2023-10-02 01:35:19 +0530

    mycode

--- 10_contextAPI/minicontext/src/App.jsx
+++ 10_contextAPI/minicontext/src/App.jsx
@@ -1,16 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+   <UserContextProvider>
 
-      <h1>HEllo World</h1>
-    </>
+    <h1>hello world</h1>
+
+    <Login/>
+    <Profile/>
+   </UserContextProvider>
+
+ 
   )
 }
 
