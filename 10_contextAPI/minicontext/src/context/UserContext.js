7b8423d77dd4653a4d6387f4f1fd1a3c6a702c93
cmrohityadav <cmrohityadav23@gmail.com>
2023-10-02 00:37:40 +0530

    context API

---
+++ 10_contextAPI/minicontext/src/context/UserContext.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const UserContext=React.createContext()
+
+
+export default UserContext;
+
+
+/**
+ * 
+1. yeh file hamesha .js me hi rhna chahiye
+2. context ko global variable samjho
+
+
+*/
