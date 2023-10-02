92424ff86b41bb9994823748da05d8aad9f3b9a7
cmrohityadav <cmrohityadav23@gmail.com>
2023-10-02 12:58:41 +0530

    done with project

--- 10_contextAPI/themeswitcher/src/App.jsx
+++ 10_contextAPI/themeswitcher/src/App.jsx
@@ -1,7 +1,7 @@
 
 import { useEffect, useState } from 'react'
 import './App.css'
-import { ThemeProvider } from '/contexts/theme'
+import { ThemeProvider } from './contexts/theme'
 import ThemeBtn from './components/ThemeBtn'
 import Card from './components/Card'
 
