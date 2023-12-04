d141118f770f189a9cd20879a18df7eff2f5f5e3
cmrohityadav <cmrohityadav23@gmail.com>
2023-12-04 15:19:22 +0530

    todo v1 just ui ready

---
+++ Project/01/todoAppV1/src/components/AddTODO.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function AddTODO() {
+  return (
+    <>
+
+<div class="row">
+            <div class="col-6">
+              <input type='text' placeholder='Wnter TODO Here'
+
+
+
+              >
+
+              </input>
+            </div>
+
+            <div class="col-4">
+              <input type="date" name="" id="" />
+            </div>
+
+
+            <div class="col-2">
+              <button type="button" class="btn btn-success">Add</button>
+            </div>
+          </div>
+    </>
+  )
+}
+
+export default AddTODO
