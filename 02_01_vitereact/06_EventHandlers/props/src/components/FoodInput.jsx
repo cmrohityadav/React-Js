80bd89eea41bedca4bc2fca1ae6b775d20e2b3d4
cmrohityadav <cmrohityadav23@gmail.com>
2023-12-10 18:48:15 +0530

    event handle

--- 02_01_vitereact/06_EventHandlers/props/src/components/FoodInput.jsx
+++ 02_01_vitereact/06_EventHandlers/props/src/components/FoodInput.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import styles from "./FoodInput.module.css"
 function FoodInput() {
+  const handleOnChange=(event)=>{
+    console.log(event.target.value)
+  }
   return (
+
+
     <>
 
-        <input 
+      <input
         type='text'
         placeholder='Enter Food Item Here'
         className={styles.foodInput}
-        onChange={(event)=> console.log(event.target.value)}
+      // onChange={(event)=> console.log(event.target.value)}
+      // or
+      onChange={handleOnChange}
 
-        />
+
+      />
 
 
     </>
