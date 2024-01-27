ef3d7d01b676c9772eff1449381494e8f126be11
cmrohityadav <cmrohityadav23@gmail.com>
2024-01-27 13:10:52 +0530

    social media ready

--- Project/04/social media/src/components/CreatePost.jsx
+++ Project/04/social media/src/components/CreatePost.jsx
@@ -1,36 +1,61 @@
-import React from 'react'
+import React, { useContext, useRef } from 'react'
+import { PostList } from '../store/PostListStore';
 
 function CreatePost() {
+
+  const { addPost } = useContext(PostList)
+
+  const userIdElement = useRef();
+  const postTitleElement = useRef();
+  const postBodyElement = useRef();
+  const reactionsElement = useRef();
+  const tagsElement = useRef();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const postBody = postBodyElement.current.value;
+    const postTitle = postTitleElement.current.value;
+    const userId = userIdElement.current.value;
+    const reactions = reactionsElement.current.value;
+    const tags = tagsElement.current.value.split(" ")
+
+    addPost(userId,postTitle,postBody,reactions,tags)
+    postBodyElement.current.value=""
+    postTitleElement.current.value=""
+  userIdElement.current.value=""
+   reactionsElement.current.value=""
+ tagsElement.current.value=""
+  }
+
   return (
     <>
-      <form className="create-post">
+      <form className="create-post" onSubmit={handleSubmit}>
 
         <div className="mb-3">
           <label htmlFor="userId" className="form-label">Enter your userID here</label>
-          <input type="text" className="form-control" id="userId" placeholder='your user id' aria-describedby="emailHelp" />
+          <input type="text" className="form-control" id="userId" placeholder='your user id' aria-describedby="emailHelp" ref={userIdElement} />
 
         </div>
         <div className="mb-3">
           <label htmlFor="title" className="form-label">Post Title</label>
-          <input type="text" className="form-control" id="title" placeholder='How are you feeling today' aria-describedby="emailHelp" />
+          <input type="text" className="form-control" id="title" placeholder='How are you feeling today' aria-describedby="emailHelp" ref={postTitleElement} />
 
         </div>
 
 
         <div className="mb-3">
           <label htmlFor="body" className="form-label">Post Content</label>
-          <textarea rows={4} type="text" className="form-control" id="body" placeholder='tell us more about it' aria-describedby="emailHelp" />
+          <textarea rows={4} type="text" className="form-control" id="body" placeholder='tell us more about it' aria-describedby="emailHelp" ref={postBodyElement} />
 
         </div>
         <div className="mb-3">
           <label htmlFor="reactions" className="form-label">Number of reactions</label>
-          <input type="number" className="form-control" id="reactions" placeholder='How many people recated to this post' aria-describedby="emailHelp" />
+          <input type="number" className="form-control" id="reactions" placeholder='How many people recated to this post' aria-describedby="emailHelp" ref={reactionsElement} />
 
         </div>
 
         <div className="mb-3">
           <label htmlFor="tags" className="form-label">Enter your tags</label>
-          <input type="text" className="form-control" id="tags" placeholder='please enter tag using space' aria-describedby="emailHelp" />
+          <input type="text" className="form-control" id="tags" placeholder='please enter tag using space' aria-describedby="emailHelp" ref={tagsElement} />
 
         </div>
 
--- Project/04/social media/src/store/PostListStore.jsx
+++ Project/04/social media/src/store/PostListStore.jsx
@@ -13,6 +13,8 @@ const postListReducer = (currPostList, action) => {
     let newPostList=currPostList
     if(action.type==='DELETE_POST'){
         newPostList=currPostList.filter((post)=>post.id!==action.payload.postId)
+    }else if(action.type==='ADD_POST'){
+            newPostList=[action.payload,...currPostList]
     }
     return newPostList;
 }
@@ -22,7 +24,23 @@ const postListReducer = (currPostList, action) => {
 
 const PostListProvider = ({ children }) => {
     const [postList, dispatchPostList] = useReducer(postListReducer,DEFAULT_POST_LIST)
-    const addPost = () => { };
+    const addPost = (userId,postTitle,postBody,reactions,tags) => { 
+
+        // console.log(`${userId} , ${postTitle},${postBody},${reactions},${tags}`)
+
+        dispatchPostList({
+            type:'ADD_POST',
+            payload:{
+                id: Date.now(),
+                title: postTitle,
+                body: postBody,
+                reactions: reactions,
+                userId: userId,
+                tags: tags
+
+            }
+        })
+    };
     const deletePost = (postId) => { 
         // console.log(`delete post called for ${postId}`)
 
