f1e60c4fe328ae327a910869994d13cd473f47f3
cmrohityadav <cmrohityadav23@gmail.com>
2024-01-29 13:13:56 +0530

    react Router

---
+++ 02_basics/17_router/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Sidebar from './components/Sidebar'
+import CreatePost from './components/CreatePost'
+import PostList from './components/PostList'
+import PostListProvider from './store/PostListStore'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+const [selectedTab,setSelectedTab]=useState('Home');
+
+
+  return (
+    <> 
+    <PostListProvider>
+    <div className="app-container">
+      <Sidebar selectedTab={selectedTab}
+      setSelectedTab={setSelectedTab}
+       />
+      <div className="content">
+        <Header />
+        {/* {selectedTab==='Home'?<PostList />: <CreatePost /> } */}
+       <Outlet></Outlet>
+        
+        <Footer />
+
+      </div>
+
+    </div>
+
+</PostListProvider>
+    </>
+  )
+}
+
+export default App
