a4b4f2ebb18bef66399c7d46db59adfbf77f523d
cmrohityadav <cmrohityadav23@gmail.com>
2024-02-01 17:39:59 +0530

    changed title

--- Project/05_passwordGenerator/passwordGenerator/src/components/Heading.jsx
+++ Project/05_passwordGenerator/passwordGenerator/src/components/Heading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Heading() {
   return (
     <>
-   <p className='text-center text-orange-50'>Password Generator</p>
+   <p className='text-center text-orange-50'>Ramndom Password Generator</p>
 
     </>
   )
